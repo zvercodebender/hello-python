267a7ae23b051985aad9ab5d20230872824cd998
Daniel Berg <berg417@gmail.com>
2021-05-21 11:13:14 -0400

    Update message - friends

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python - Dan!"
+    return "Hello friends from Python!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
