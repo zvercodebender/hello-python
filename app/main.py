72290d4f94f106f9d9ddfb444528a03e8e93052a
Rick Broker <rbroker@xebialabs.com>
2021-09-09 09:19:05 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello it's Thursday says Python!"
+    return "Hello Digital.ai!<br>#GoTeam!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
