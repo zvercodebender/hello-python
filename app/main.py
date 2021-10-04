3f99687cb0a848ff48a4ec2bd20a63323a94e111
zvercodebender <rbroker@digital.ai>
2021-10-04 15:17:10 -0400

    go team

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Digital.ai!<br>TEST"
+    return "Hello Digital.ai!<br>#GoTeam!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
