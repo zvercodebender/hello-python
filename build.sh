c0299c0314df76bfc1d1fa7881c530686c60d6d9
Rick Broker <rbroker@blackpug.net>
2022-03-25 09:08:06 -0400

    run as pipeline

--- build.sh
+++ build.sh
@@ -15,13 +15,17 @@ docker build -t ${IMAGE} -f Dockerfile .
 docker login --username $DOCKER_USER --password $DOCKER_PASSWORD docker.io
 echo "docker push ${IMAGE}"
 docker push ${IMAGE}
-curl -k -X POST "${CB_FLOW}/rest/v1.0/projects/MyProject/releases?releaseName=%20Hello%20Python%20Release%20${TAG}&pipelineName=hello-python&pipelineProjectName=MyProject" \
-    -u ${CB_USER}:${CB_PASSWD} \
-    -H "accept: application/json" \
-    -d "{\"actualParameter\":[{\"actualParameterName\":\"appVersion\",\"value\":\"${TAG}\"}]}"
+# curl -k -X POST "${CB_FLOW}/rest/v1.0/projects/MyProject/releases?releaseName=%20Hello%20Python%20Release%20${TAG}&pipelineName=hello-python&pipelineProjectName=MyProject" \
+#     -u ${CB_USER}:${CB_PASSWD} \
+#     -H "accept: application/json" \
+#     -d "{\"actualParameter\":[{\"actualParameterName\":\"appVersion\",\"value\":\"${TAG}\"}]}"
 
-curl -k -X POST "${CB_FLOW}/rest/v1.0/releases?projectName=MyProject&releaseName=%20Hello%20Python%20Release%20${TAG}" \
+# curl -k -X POST "${CB_FLOW}/rest/v1.0/releases?projectName=MyProject&releaseName=%20Hello%20Python%20Release%20${TAG}" \
+#      -u ${CB_USER}:${CB_PASSWD} \
+#      -H "accept: application/json" \
+#      -d "{}"
+
+curl -k -X POST “${CB_FLOW}/rest/v1.0/pipelines?pipelineName=hello-python&projectName=MyProject” \
      -u ${CB_USER}:${CB_PASSWD} \
-     -H "accept: application/json" \
-     -d "{}"
-
+     -H “accept: application/json” \
+     -d “{\“actualParameter\“:[{\“actualParameterName\“:\“appVersion\“,\“value\“:\“${TAG}\“}]}”
