2905d78b6c54e841559244d0a6a8031b4ea7f0b7
Rick Broker <rbroker@blackpug.net>
2022-03-29 12:04:57 -0400

    add container build back

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ fi
 echo "Version = ${TAG}"
 IMAGE="${REPO}:${TAG}"
 
+echo "docker build -t ${IMAGE} -f Dockerfile ."
+docker build -t ${IMAGE} -f Dockerfile .
+docker login --username $DOCKER_USER --password $DOCKER_PASSWORD docker.io
+echo "docker push ${IMAGE}"
+docker push ${IMAGE}
  
 curl -k -X POST "${CB_FLOW}/rest/v1.0/projects/MyProject/catalogs/Hello-Python/catalogItems/Hello%20Python?request=runCatalogItem" \
      -u ${CB_USER}:${CB_PASSWD} \
