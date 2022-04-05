df0e570282914e023cf35dc1b37fbb5210fe218c
Rick Broker <rbroker@cloudbees.com>
2022-04-05 15:01:51 -0400

    updates

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-export CLI_VERSION="10.0.0"
 export REPO="rbroker/hello-py"
-TAG="$(git rev-parse --short HEAD)"
-#export VERSION="3.1.1"
+TAG="$(date +%y.%m.%d-%H%M%S)"
 if [ -z "${TAG}" ]
 then
   echo "VERSION not set"
@@ -17,17 +15,13 @@ docker build -t ${IMAGE} -f Dockerfile .
 docker login --username $DOCKER_USER --password $DOCKER_PASSWORD docker.io
 echo "docker push ${IMAGE}"
 docker push ${IMAGE}
-sed "s/{{image_tag}}/${TAG}/g" kubernetes/deployment.yaml > kubernetes/hello-python.yaml
 
-if [ -z "${GET_LOCAL}" ]
-then
-  xl apply --file dai-deploy/hello-python-deploy.yaml --values version=$TAG
-else
-  curl -LO https://dist.xebialabs.com/public/xl-cli/$CLI_VERSION/linux-amd64/xl
-  chmod +x xl
-  ./xl apply --xl-deploy-url=$XLD_URL --xl-deploy-username=$XLD_USER --xl-deploy-password=$XLD_PASSWD --file dai-deploy/hello-python-deploy.yaml --values version=$TAG
-  ./xl apply --xl-release-url=$XLR_URL --xl-release-username=$XLD_USER --xl-release-password=$XLD_PASSWD -d --file dai-deploy/start-release.yaml
-  rm xl
-fi
-#rm x.yaml
-rm kubernetes/hello-python.yaml
+curl -k -X POST "${CB_FLOW}/rest/v1.0/projects/MyProject/releases?releaseName=%20Hello%20Python%20Release%20Rest%20API&pipelineName=hello-python&pipelineProjectName=MyProject" \
+     -u ${CB_USER}:${CB_PASSWD} \
+     -H "accept: application/json" \
+     -d "{\"actualParameter\":[{\"actualParameterName\":\"appVersion\",\"value\":\"dumb\"}]}"
+
+curl -k -X POST "${CB_FLOW}/rest/v1.0/releases?projectName=MyProject&releaseName=%20Hello%20Python%20Release%20Rest%20API" \
+     -u ${CB_USER}:${CB_PASSWD} \
+     -H "accept: application/json" \
+     -d "{}"
