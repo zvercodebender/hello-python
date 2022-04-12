5e28ab1f9e5158b16396ac22d7e59c7756b6ea2d
Rick Broker <rbroker@cloudbees.com>
2022-04-12 13:16:39 -0400

    Go bucks

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body>Hello Cloudbees!<br><h1>#GoTeam!</h1></body></html>"
+    return "<html><body>Hello Cloudbees!<br><h1>#OSU!</h1></body></html>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
